f5c1ab915d1252a9a1805f28b5fe2d0c257d1360
Stefan Neubert <Stefan.Neubert@hpi.de>
2021-05-11 10:03:46 +0200

    Rewrite loadtest for yard v2. Update dependencies.

--- yard-server/README.md
+++ yard-server/README.md
@@ -2,6 +2,14 @@
 
 This is the server component for Yard.
 
+## Loadtesting
+
+Have a server running, start a room there and then run
+
+```sh
+npm run loadtest -- --endpoint {ws[s]://SERVER_ADDRESS:PORT} --room {ROOM_ID}  --numClients {NUMBER_OF_CLIENTS}
+```
+
 ## Local commands
 
 Usually you should use the commands provided on the [project's top level](../README.md).
@@ -9,24 +17,24 @@ If you feel like running local commands - here they are:
 
 Develop with
 
-```
+```sh
 npm run dev
 ```
 
 Lint with
 
-```
+```sh
 npm run lint
 ```
 
 Build with
 
-```
+```sh
 npm run build
 ```
 
 Run the built server with
 
-```
+```sh
 npm start
 ```
--- yard-server/loadtest/loadtest.ts
+++ yard-server/loadtest/loadtest.ts
@@ -1,54 +1,54 @@
-import { Room, Client } from "colyseus.js";
+import { MovePayload } from "../../common/pixelInterface";
+import { YardState } from "../src/rooms/schema/YardState";
+import { Client, Room } from "colyseus.js";
 
-export function requestJoinOptions (this: Client, i: number) {
-    return { };
+export function requestJoinOptions(this: Client): Record<string, never> {
+  return {};
 }
 
 let interval: NodeJS.Timeout | undefined;
-let oldX: number = 0;
-let oldY: number = 0;
+let angle: Record<string, number> = { };
 
-export function onJoin(this: Room) {
-    console.log(this.sessionId, "joined.");
+export function onJoin(this: Room): void {
+  console.log(this.sessionId, "joined.");
 
-    interval = setInterval(() => sendRandomMove(this), 100);
+  interval = setInterval(() => sendRandomMove(this), 20);
 }
 
-export function onLeave(this: Room) {
-    console.log(this.sessionId, "left.");
-    if (interval) {
-        clearInterval(interval);
-    }
+export function onLeave(this: Room): void {
+  console.log(this.sessionId, "left.");
+  if (interval) {
+    clearInterval(interval);
+  }
 }
 
-export function onError(this: Room, err: any) {
-    console.log(this.sessionId, "!! ERROR !!", err.message);
+export function onError(this: Room, err: Error): void {
+  console.log(this.sessionId, "!! ERROR !!", err.message);
 }
 
-function sendRandomMove(room: Room) {
-    room.send("move", randomMove());
+function sendRandomMove(room: Room): void {
+  const pixel = Array.from((room.state as YardState).pixels.entries())
+    .filter(([ , pixel ]) => pixel.player === room.sessionId)[0];
+  if (pixel) {
+    room.send("move", randomMove(pixel[0]));
+  }
 }
 
-function randomMove() {
-    const speed = Math.random() * 4 + 1;
-    const move = {
-        x: randomDirection(oldX) * speed,
-        y: randomDirection(oldY) * speed,
-    }
-    oldX = move.x;
-    oldY = move.y;
-    return move;
+function randomMove(pixel: string): MovePayload {
+  angle[pixel] = randAngle(angle[pixel] ?? 0);
+
+  return {
+    p: pixel,
+    a: Math.floor((angle[pixel] * (180 / Math.PI) + 360) % 360 / 3), // transport data is non-negative angle in 1/3 degrees
+  };
 }
 
-function randomDirection(old: number) {
-    const rand = Math.random();
-    if (rand < 3/9) {
-        return old;
-    } else if (rand < 5/9) {
-        return -1
-    } else if (rand < 7/9) {
-        return 1;
-    } else {
-        return 0;
-    }
+function randAngle(oldAngle: number) {
+  const rand = Math.random();
+  const newAngle = Math.random() * 2 * Math.PI;
+  if (rand < 99 / 100) {
+    return oldAngle;
+  } else {
+    return newAngle;
+  }
 }
