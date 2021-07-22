7053830e593084821935ae60c13000179f33881c
Stefan Neubert <Stefan.Neubert@hpi.de>
2021-07-22 17:39:38 +0200

    Beautify nametags.

--- yard-client/src/views/yard/YPixel.vue
+++ yard-client/src/views/yard/YPixel.vue
@@ -1,7 +1,11 @@
 <template>
-  <g class="player" ref="group" :style="dynamicStyle" :class="dynamicClass">
+  <g class="player" ref="group" :style="playerStyle" :class="playerClass">
     <rect class="pixel" :x="-pixelSize/2" :y="-pixelSize/2" :width="pixelSize" :height="pixelSize" @click="selectPixel"></rect>
-    <text text-anchor="middle" dominant-baseline="central">{{playerName}}</text>
+    <g class="player-name" :style="labelStyle">
+      <path :d="labelPath"></path>
+      <rect :x="-textWidth/2" :y="-textHeight/2" :width="textWidth" :height="textHeight" rx="5" ry="100%"></rect>
+      <text ref="tag">{{playerName}}</text>
+    </g>
     <rect class="pixel-highlight" v-if="pixelId === $yardAPI.store.pixelHighlight"
           :x="-pixelSize/2" :y="-pixelSize/2" :width="pixelSize" :height="pixelSize" ></rect>
   </g>
@@ -25,6 +29,9 @@
         required: true,
       },
     },
+    data() {
+      return { textWidth: 0, textHeight: 0 };
+    },
     computed: {
       pixelSize(): number {
         return this.$yardAPI.store.settings.pixelSize;
@@ -32,7 +39,7 @@
       playerName(): string {
         return this.$yardAPI.store.players.get(this.pixel.player)?.name ?? "";
       },
-      dynamicStyle(): Record<string, string> {
+      playerStyle(): Record<string, string> {
         return {
           "--player-color": this.pixel.color,
         };
@@ -40,7 +47,7 @@
       selectable(): boolean {
         return this.pixel.player === this.$yardAPI.store.sessionId;
       },
-      dynamicClass(): Record<string, boolean> {
+      playerClass(): Record<string, boolean> {
         return {
           "show-name": this.$yardAPI.store.localSettings.showNames,
           "selectable": this.selectable,
@@ -52,12 +59,36 @@
           y: this.pixel.y,
         };
       },
+      labelStyle(): Record<string, string> {
+        return {
+          transform: `translate(${this.labelXPosition}px, ${this.labelYPosition}px)`,
+        };
+      },
+      labelXPosition(): number {
+        const ratio = this.pixel.x / this.$yardAPI.store.settings.canvasWidth;
+        return (ratio - 0.5) * -(this.textWidth + 0.5 * this.pixelSize);
+      },
+      labelYPosition(): number {
+        return (0.5 * this.pixelSize + 2 * this.textHeight) * this.labelYDirection;
+      },
+      labelYDirection(): number {
+        return this.pixel.y / this.$yardAPI.store.settings.canvasHeight < 0.15 ? 1 : -1;
+      },
+      labelPath(): string {
+        return `M0 ${this.textHeight / -2 * this.labelYDirection} V${-this.labelYPosition} L${-this.labelXPosition} ${-this.labelYPosition}`;
+      },
     },
     methods: {
       selectPixel() {
         if (this.selectable) {
           this.$yardAPI.controller.activePixel = this.pixelId;
         }
+      },
+      detectTextSize() {
+        const tag = this.$refs.tag as SVGTextElement;
+        const bbox = tag.getBBox();
+        this.textWidth = bbox.width + 8;
+        this.textHeight = bbox.height;
       },
     },
     watch: {
@@ -72,6 +103,10 @@
     },
     mounted() {
       gsap.set(this.$el, { translateX: this.pos.x, translateY: this.pos.y, duration: 0.3 });
+      this.$nextTick(() => this.detectTextSize());
+    },
+    updated() {
+      this.$nextTick(() => this.detectTextSize());
     },
   });
 </script>
@@ -84,13 +119,27 @@
     &.selectable rect.pixel {
       cursor: pointer;
     }
-    text {
+    .player-name {
       pointer-events: none;
       opacity: 0;
       transition: opacity 200ms ease-in-out;
-      filter: drop-shadow(0 0 3px white);
+      path {
+        stroke: var(--player-color);
+        fill: none;
+      }
+      rect {
+        fill: rgba(255,255,255,0.5);
+        stroke: var(--player-color);
+        filter: drop-shadow(0 0 3px white);
+      }
+      text {
+        dominant-baseline: central;
+        text-anchor: middle;
+        font-size: 0.8rem;
+        fill: var(--player-color);
+      }
     }
-    rect.pixel:hover + text, &.show-name text {
+    rect.pixel:hover + .player-name, &.show-name .player-name {
       opacity: 1;
     }
   }
