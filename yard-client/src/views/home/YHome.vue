Steditor/yard

commit 7e87c690b6ddae55365d68f1e4b95de6014ef4ed

Use accordion instead of tabs for create/join form. (Tabs were too wide for mobile devices)

yard-client/src/styles/main.scss
@@ -29,7 +29,7 @@ p {
 
 .y-flex-center {
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
 }

yard-client/src/views/home/YHome.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="y-full-size y-flex-center">
     <div class="p-card login-card">
-      <TabView>
-        <TabPanel>
+      <Accordion :active-index="0">
+        <AccordionTab>
           <template #header>
             <i class="pi pi-sign-in"></i>
             <span class="ml-3">Join a Yard</span>
           </template>
           <YJoinPanel />
-        </TabPanel>
-        <TabPanel>
+        </AccordionTab>
+        <AccordionTab>
           <template #header>
             <i class="pi pi-plus"></i>
             <span class="ml-3">Create a Yard</span>
           </template>
           <YCreatePanel />
-        </TabPanel>
-      </TabView>
+        </AccordionTab>
+      </Accordion>
     </div>
   </div>
 </template>
@@ -24,15 +24,15 @@
 <script lang="ts">
   import { defineComponent } from "vue";
 
-  import TabView from "primevue/tabview";
-  import TabPanel from "primevue/tabpanel";
+  import Accordion from "primevue/accordion";
+  import AccordionTab from "primevue/accordiontab";
 
   import YJoinPanel from "./YJoinPanel.vue";
   import YCreatePanel from "./YCreatePanel.vue";
 
   export default defineComponent({
     name: "YHome",
-    components: { YCreatePanel, YJoinPanel, TabView, TabPanel },
+    components: { YCreatePanel, YJoinPanel, Accordion, AccordionTab },
   });
 </script>
 

commit 69814dfe5b4327f39150d52c54a9f957c5300998

Fix yard scaling. (regression from 7e87c690b6ddae55365d68f1e4b95de6014ef4ed)

yard-client/src/styles/main.scss
@@ -29,7 +29,7 @@ p {
 
 .y-flex-center {
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 }

yard-client/src/views/home/YHome.vue
@@ -38,7 +38,7 @@
 
 <style lang="scss" scoped>
   .login-card {
-    width: 500px;
+    max-width: 500px;
   }
   i, span {
     vertical-align: middle;