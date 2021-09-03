7d6d8c78af1ea2b9eddb308b6cc070b859a0c288
Stefan Neubert <Stefan.Neubert@hpi.de>
2021-09-03 12:00:26 +0200

    Add QRCode to moderation view as alternative to sharing the text-link.

--- yard-client/src/views/moderation/YModInfo.vue
+++ yard-client/src/views/moderation/YModInfo.vue
@@ -8,7 +8,11 @@
       <p>
         You're currently moderating yard <YCopyable :content="roomId" />.
         Others can join the yard with this key.
-        They can also use the link <YCopyable :content="yardLink"><RouterLink :to="yardRoute" target="_blank">{{yardLink}}</RouterLink></YCopyable>.
+        They can also use the link <YCopyable :content="yardLink"><RouterLink :to="yardRoute" target="_blank">{{yardLink}}</RouterLink></YCopyable>
+        or scan <a href="#" @click="showQRCode = true">this QR-Code</a>.
+        <Dialog header="Yard QRCode" v-model:visible="showQRCode" modal dismissable-mask class="qr-dialog">
+          <YQRCode :content="yardLink" :options="{ scale: 20 }" />
+        </Dialog>
       </p>
       <p>If you want to allow others to moderate this yard, send them the moderation key in addition to the yard link: <YCopyable :content="moderationKey" secret />.</p>
     </template>
@@ -20,12 +24,19 @@
   import { RouteLocationRaw } from "vue-router";
 
   import Card from "primevue/card";
+  import Dialog from "primevue/dialog";
 
   import YCopyable from "@/components/YCopyable.vue";
+  import YQRCode from "@/components/YQRCode.vue";
 
   export default defineComponent({
     name: "YModInfo",
-    components: { YCopyable, Card },
+    components: { YQRCode, YCopyable, Card, Dialog },
+    data() {
+      return {
+        showQRCode: false,
+      };
+    },
     computed: {
       roomId(): string {
         return this.$yardAPI.store.roomId ?? "";
@@ -44,3 +55,9 @@
     },
   });
 </script>
+
+<style lang="scss">
+  .qr-dialog .p-dialog-content {
+    display: flex;
+  }
+</style>
