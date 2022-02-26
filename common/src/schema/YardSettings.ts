fd3abd8b7d587eec5e3934d744ccf86777d5ee25
Stefan Neubert <Stefan.Neubert@hpi.de>
2022-02-26 20:31:33 +0100

    Update dependencies

--- common/src/schema/YardSettings.ts
+++ common/src/schema/YardSettings.ts
@@ -2,7 +2,7 @@
 // THIS FILE HAS BEEN GENERATED AUTOMATICALLY
 // DO NOT CHANGE IT MANUALLY UNLESS YOU KNOW WHAT YOU'RE DOING
 // 
-// GENERATED USING @colyseus/schema 1.0.33
+// GENERATED USING @colyseus/schema 1.0.34
 // 
 
 import { Schema, type, ArraySchema, MapSchema, SetSchema, DataChange } from '@colyseus/schema';
